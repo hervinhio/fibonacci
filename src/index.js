be716b941338a73f3e2dc809a18dc577869d455b
root <root@Cambridge.localdomain>
2026-04-24 11:25:01 +0000

    Implement fibonnacy function in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-function fibonacci(index) {}
+function fibonnacy(index) {
+  if (index === 0) return 0;
+  if (index === 1) return 1;
+  return fibonnacy(index - 1) + fibonnacy(index - 2);
+}
 
-module.exports = fibonacci;
+module.exports = fibonnacy;
